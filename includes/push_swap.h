9696513c6079d57e4d839e8fb8987bc45b6ed7d0
Slathouwers <sander.lathouwers@live.be>
2021-10-09 11:07:31 +0000

    libft header include updated

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,11 +6,34 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 09:25:00 by slathouw          #+#    #+#             */
-/*   Updated: 2021/10/09 09:26:07 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/10/09 10:10:37 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+#include "../libftprintf/includes/ft_printf_bonus.h"
+
+/*ARRAY STRUCT AND FUNCTIONS*/
+
+typedef struct s_stack
+{
+	int				content;
+	int				hash;
+	struct s_stack	*next;
+}	t_stack;
+
+typedef struct s_frame
+{
+	struct s_stack	*st_a;
+	int				a_len;
+	struct s_stack	*st_b;
+	int				b_len;
+}	t_frame;
+
+/*PUSH_SWAP COMMANDS*/
+
+/*SORTING FUNCTIONS*/
 
 #endif
---
+++ srcs/push_swap.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 09:57:09 by slathouw          #+#    #+#             */
+/*   Updated: 2021/10/09 10:46:29 by slathouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	(void) argv;
+	ft_printf("number of args = %d", argc);
+	//TODO: parse arguments to stack a
+	//		if argc==2 and argv[1] is not a number -> parse string as arguments (split by spaces)
+	//		else parse argv array as seperate numbers
+	//TODO: check for duplicates 
+	//TODO: get command list for stack sorting
+	//TODO: free stacks
+	//TODO: print commands
+	//TODO: free commandlist
+	return (0);
+}
