bc9f145a6bc34899eec384444beabca462f89419
Slathouwers <sander.lathouwers@live.be>
2021-10-11 13:05:48 +0000

    added macro's for accessing stack element props

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 09:25:00 by slathouw          #+#    #+#             */
-/*   Updated: 2021/10/09 10:10:37 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/10/11 12:16:26 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,35 @@
 
 #include "../libftprintf/includes/ft_printf_bonus.h"
 
+# define TO_EL_PTR(el)	((t_stack_el *)el)
+
+# define S_EL_HASH(el)		((t_stack_el *)el->content)->hash
+# define S_EL_NUMBER(el)		((t_stack_el *)el->content)->number
+
 /*ARRAY STRUCT AND FUNCTIONS*/
 
-typedef struct s_stack
+typedef struct s_stack_el
 {
-	int				content;
-	int				hash;
-	struct s_stack	*next;
-}	t_stack;
+	int	number;
+	int	hash;
+}		t_stack_el;
+
+typedef t_list t_stack;
+
+typedef struct s_cmdlist
+{
+	int		count;
+	char	*cmd_str;
+}			t_cmdlist;
 
 typedef struct s_frame
 {
-	struct s_stack	*st_a;
-	int				a_len;
-	struct s_stack	*st_b;
-	int				b_len;
-}	t_frame;
+	struct s_stack		*st_a;
+	int					a_len;
+	struct s_stack		*st_b;
+	int					b_len;
+	struct s_cmdlist	*cmds;
+}						t_frame;
 
 /*PUSH_SWAP COMMANDS*/
 
