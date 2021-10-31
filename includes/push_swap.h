baff7630c4f1c2256f019c70e78edc4d8d0315cb
Slathouwers <sander.lathouwers@live.be>
2021-10-31 06:31:02 +0000

    added ptr_conversions and swap ops / removed ptr macros

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 09:25:00 by slathouw          #+#    #+#             */
-/*   Updated: 2021/10/19 07:01:19 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/10/31 06:28:49 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 # define PUSH_SWAP_H
 
 # include "../libftprintf/includes/ft_printf_bonus.h"
-
-# define TO_EL_PTR(el)	((t_stack_el *)el)
-
-# define S_EL_HASH(el)		((t_stack_el *)el->content)->hash
-# define S_EL_NUMBER(el)		((t_stack_el *)el->content)->number
 
 /*ARRAY STRUCT AND FUNCTIONS*/
 
@@ -37,6 +32,11 @@ typedef struct s_frame
 	t_stack		*st_b;
 	int			b_to_sort;
 }				t_frame;
+
+/*PTR_CONVERSIONS*/
+t_stack_el	*to_el_ptr(void *el);
+int			s_el_hash(t_stack *el);
+int			s_el_number(t_stack *el);
 
 /*PARSING FUNCTIONS*/
 int			check_is_args_num(int argc, char **args);
@@ -58,9 +58,16 @@ void		rr(t_frame *f, char c);
 	/*PUSH*/
 void		push(t_stack **from, t_stack **to);
 void		p(t_frame *f, char c);
+	/*SWAP*/
+void		swap(t_stack **s);
+void		s(t_frame *f, char c);
+void		ss(t_frame *f);
 
 /*SORTING FUNCTIONS*/
 void		radix_sort(t_frame *f);
+void		small_sort(t_frame *f);
+
+/*SMALL SORT*/
 
 /* RADIX_SORT*/
 int			is_stack_bit_sorted(t_frame *f, int hash_bit);
@@ -69,7 +76,7 @@ int			max_hash_bit(int max_hash);
 
 /*RADIX UTILS*/
 int			get_bit(int number, int significance);
-void		swap(int *a, int *b);
+void		ft_swap(int *a, int *b);
 void		selection_sort(int *arr, int n);
 int			idx_from_arr(int *arr, int n, int to_find);
 void		hash_el_to_index(t_stack *s);
