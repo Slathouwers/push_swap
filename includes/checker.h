3e1b2dd1706c3a1de19526995a19e233c9e08299
Slathouwers <sander.lathouwers@live.be>
2021-11-10 11:58:31 +0000

    updated headerfile and added stack utility functions

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 11:01:02 by slathouw          #+#    #+#             */
-/*   Updated: 2021/11/10 11:01:37 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/11/10 11:54:42 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,53 @@
 
 # include "../libftprintf/includes/ft_printf_bonus.h"
 
+/*ARRAY STRUCT AND FUNCTIONS*/
 
+typedef struct s_stack_el
+{
+	int	number;
+	int	hash;
+}		t_stack_el;
+
+typedef t_list	t_stack;
+
+typedef struct s_frame
+{
+	t_stack		*st_a;
+	int			a_to_sort;
+	t_stack		*st_b;
+	int			b_to_sort;
+}				t_frame;
+
+/*PTR_CONVERSIONS*/
+t_stack_el	*to_el_ptr(void *el);
+int			s_el_hash(t_stack *el);
+int			s_el_number(t_stack *el);
+
+/*STACK UTILS*/
+int			get_bit(int number, int significance);
+void		ft_swap(int *a, int *b);
+void		selection_sort(int *arr, int n);
+int			idx_from_arr(int *arr, int n, int to_find);
+void		hash_el_to_index(t_stack *s);
+
+/*PARSING FUNCTIONS*/
+int			check_is_args_num(int argc, char **args);
+int			split_len(char **split);
+t_stack_el	*new_stack_el(int number);
+t_stack		*parse_args(int argc, char **args);
+t_stack		*parse_string(char *args);
+
+/*PARSING UTILS*/
+int			ft_isnbr(char *str);
+int			ft_isint(const char *str);
+int			has_duplicates(t_stack *stack);
+int			is_sorted(t_stack *s);
+
+/*PRINTING FUNCTIONS*/
+void		print_stack_el(void *el);
+void		print_stack(t_stack *s);
+void		print_arr(int *arr, int n);
+void		print_stacks(t_frame *f);
 
 #endif
---
+++ srcs_checker/stack_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/15 11:44:17 by slathouw          #+#    #+#             */
+/*   Updated: 2021/11/10 11:49:53 by slathouw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/checker.h"
+
+int	get_bit(int number, int significance)
+{
+	return (number >> significance & 1);
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	selection_sort(int *arr, int n)
+{
+	int	i;
+	int	j;
+	int	min_index;
+
+	i = -1;
+	while (++i < n - 1)
+	{
+		min_index = i;
+		j = i;
+		while (++j < n)
+			if (arr[j] < arr[min_index])
+				min_index = j;
+		ft_swap(&arr[min_index], &arr[i]);
+	}
+}
+
+int	idx_from_arr(int *arr, int n, int to_find)
+{
+	int	i;
+
+	i = -1;
+	while (++i < n && arr[i] != to_find)
+		;
+	return (i);
+}
+
+void	hash_el_to_index(t_stack *s)
+{
+	int		*numbers;
+	t_list	*iter_ptr;
+	int		n;
+	int		i;
+
+	iter_ptr = s;
+	i = 0;
+	n = ft_lstsize(s);
+	numbers = ft_calloc(sizeof(int), n);
+	if (!numbers)
+		return ;
+	while (iter_ptr)
+	{
+		numbers[i++] = s_el_number(iter_ptr);
+		iter_ptr = iter_ptr->next;
+	}
+	selection_sort(numbers, n);
+	iter_ptr = s;
+	while (iter_ptr)
+	{
+		((t_stack_el *)iter_ptr->content)->hash
+			= idx_from_arr(numbers, n, s_el_number(iter_ptr));
+		iter_ptr = iter_ptr->next;
+	}
+	free(numbers);
+}
